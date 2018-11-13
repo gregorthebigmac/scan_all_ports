718395da6accf4d55c7c6addce70c45b02e63c2f
gray fox <gfox@localhost.localdomain>
2018-11-13 08:45:55 -0600

    update

---
+++ host.hpp
@@ -0,0 +1,22 @@
+#ifndef HOST_HPP
+#define HOST_HPP
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+class host {
+public:
+
+	host();	// ctor
+	~host();	// dtor
+	void convert_and_set_ip_address(std::string address);
+	void convert_and_populate_open_ports(std::vector<std::string>ports);
+	
+private:
+
+	int m_ip_address[4];
+	std::vector<int> m_open_ports;
+};
+
+#endif
